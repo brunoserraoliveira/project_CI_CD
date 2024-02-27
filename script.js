9c765ecd0e2bc4419a224c5635314bd1c47ecbaf
Bruno Oliveira <brunoserrasilva@gmail.com>
2024-02-27 08:14:04 -0300

    Mudando a msg de console.log para alert

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // script.js
 
 document.getElementById('meuBotao').addEventListener('click', function() {
-    console.log('Botão clicado! Teste de CI/CD bem-sucedido.');
+    alert('Botão clicado! Teste de CI/CD bem-sucedido.');
 });
